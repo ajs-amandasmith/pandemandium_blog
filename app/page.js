94ff551abc3976c1a77c9ac0b42ab4e6f2cd6c66
Amanda Smith <ajs.amandasmith@gmail.com>
2025-08-06 14:40:11 -0400

    Added Layout component

--- app/page.js
+++ app/page.js
@@ -1,9 +1,5 @@
 import { PostCard, Categories, PostWidget } from './components';
 
-export const metadata = {
-  title: 'Pandemandium'
-}
-
 const posts = [
   { title: 'React Testing', excerpt: 'Learn React Testing' },
   { title: 'React with Tailwind', excerpt: 'Learn React with Tailwind' }
