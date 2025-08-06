bf361c70c0c5f4fc4ba351ada630f806b0889768
Amanda Smith <ajs.amandasmith@gmail.com>
2025-08-06 14:08:10 -0400

    Setup Tailwind

--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+import { PostCard, Categories, PostWidget } from './components';
+
 export const metadata = {
   title: 'Pandemandium'
 }
@@ -12,16 +14,14 @@ export default function Home() {
     <div className="container mx-auto px-10 mb-8">
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div>
-          {posts.map((post, index) => (
-            <div className='lg:col-span-8 col-span-1'>
-              {post.title}
-              {post.excerpt}
-            </div>
-          ))} 
+          <div className='col-span-1 lg:col-span-8'>
+            {posts.map((post, index) => <PostCard post={post} key={post.title} />)} 
+          </div>
         </div>
-        <div className='lg:col-span-4 col-span-1'>
-          <div className='lg:sticky relative top-8'>
-
+        <div className='col-span-1 lg:col-span-4'>
+          <div className='relative top-8 lg:sticky'>
+            <PostWidget />
+            <Categories />
           </div>
         </div>
       </div>
